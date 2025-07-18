23e6641cca969994f92a08113b9c052750505b52
您的姓名 <>
2025-07-18 20:58:10 +0800

    Initial commit

--- day1-java/src/day1/try1.java
+++ day1-java/src/day1/try1.java
@@ -1,4 +1,14 @@
 package day1;
 
 public class try1 {
+    public static void main(String[] args) {
+        int x;//变量类型不可改变
+        x=0;
+        while(x<10){
+            System.out.println(x);
+            x=x+1;
+        }
+    }
+}
+//代码在运行前就进行了验证（与python不同
 }
